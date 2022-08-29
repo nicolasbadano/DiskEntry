d52efdef6c1b1475fc6b201b6761e13b433450c6
Nicolás Diego Badano <nicolas.d.badano@gmail.com>
2022-08-29 08:54:48 -0300

    Add missing files

---
+++ OpenFOAM-v2112/tank15-t153-M5-AMR/prepare.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+. /usr/lib/openfoam/openfoam2112/etc/bashrc
+
+rm -r constant/polyMesh
+rm -r 0
+rm -r processor* postProcessing
+
+blockMesh
+surfaceFeatureExtract
+snappyHexMesh -overwrite
+renumberMesh -overwrite
+
+cp -r 0.model 0
+setFields
